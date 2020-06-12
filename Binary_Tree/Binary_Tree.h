864dede953b99c6af9a54cb4fe14ab155bf900d5
Sadham-hussian <sadhamhussian2020@gmail.com>
2020-06-12 21:53:05 +0530

    Binary_Tree: Add members to Binary_Tree

--- Binary_Tree/Binary_Tree.h
+++ Binary_Tree/Binary_Tree.h
@@ -22,6 +22,16 @@ class Binary_Tree
 		queue <struct Node*> node_queue;
 		int count;
 		void insert_key(unsigned long long int);
+
+	public:
+		Binary_Tree();
+		Binary_Tree(int);
+		struct Node* get_root() const;
+		int get_height(struct Node* node) const;
+		struct Node* new_node(unsigned long long int);
+		void insert(unsigned long long int key);
+		void level_order_traversal(struct Node* root) const;
+		virtual ~Binary_Tree();
 }
 
 #endif
